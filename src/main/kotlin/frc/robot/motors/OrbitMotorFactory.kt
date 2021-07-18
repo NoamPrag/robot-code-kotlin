194e4360683046ca970b1b10292b4410e66fc777
Noam Prag <noam.prag0@gmail.com>
2021-07-18 22:11:29 +0300

    Refactored OrbitMotorFactory

--- src/main/kotlin/frc/robot/motors/OrbitMotorFactory.kt
+++ src/main/kotlin/frc/robot/motors/OrbitMotorFactory.kt
@@ -1,15 +1,9 @@
 package frc.robot.motors
 
 object OrbitMotorFactory {
-    fun spark(data: MotorData): OrbitMotor {
-        return OrbitSpark(data)
-    }
+    fun spark(data: MotorData): OrbitMotor = OrbitSpark(data)
 
-    fun talonSRX(data: MotorData): OrbitMotor {
-        return OrbitTalon(data)
-    }
+    fun talonSRX(data: MotorData): OrbitMotor = OrbitTalon(data)
 
-    fun falcon(data: MotorData): OrbitMotor {
-        return OrbitFalcon(data)
-    }
+    fun falcon(data: MotorData): OrbitMotor = OrbitFalcon(data)
 }
