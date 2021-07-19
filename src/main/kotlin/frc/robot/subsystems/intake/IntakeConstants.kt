a52b8783f3897b0a7bc48a0f415422116403b530
Noam Prag <noam.prag0@gmail.com>
2021-07-19 15:03:11 +0300

    Added visibillity modifiers

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
@@ -5,14 +5,14 @@ import frc.robot.motors.OrbitMotorFactory
 
 import edu.wpi.first.wpilibj.DoubleSolenoid
 
-const val defaultSpeed: Float = 1f
+internal const val defaultSpeed: Float = 1f
 
-val motorData = MotorData(1, invertMotor = false, invertSensor = false, motorPerPhysicalUnitsRatio = 1f)
-val motor = OrbitMotorFactory.falcon(motorData)
+private val motorData = MotorData(1, invertMotor = false, invertSensor = false, motorPerPhysicalUnitsRatio = 1f)
+internal val motor = OrbitMotorFactory.falcon(motorData)
 
 
 
 // TODO: check real values
-const val pistonForwardChannel: Int = 0
-const val pistonReverseChannel: Int = 1
-val piston = DoubleSolenoid(pistonForwardChannel, pistonReverseChannel)
+private const val pistonForwardChannel: Int = 0
+private const val pistonReverseChannel: Int = 1
+internal val piston = DoubleSolenoid(pistonForwardChannel, pistonReverseChannel)
