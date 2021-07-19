af18c147c4b85b870240abdce56f28fb33af475e
Noam Prag <noam.prag0@gmail.com>
2021-07-19 14:45:41 +0300

    Converted intake states to objects instead of enums

--- src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/Intake.kt
@@ -3,7 +3,6 @@ package frc.robot.subsystems.intake
 import frc.robot.subsystems.RobotState
 import frc.robot.subsystems.Subsystem
 
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value
 import frc.robot.motors.MotorControlMode
 
 object Intake : Subsystem {
@@ -11,22 +10,19 @@ object Intake : Subsystem {
     override fun notifyRobotState(state: RobotState) {
         val processedState = processState(state)
 
-        val motorVelocity = getMotorVel(processedState)
+        val motorVelocity = processedState.getMotorOutput()
 
-        val pistonMode = getPistonMode(processedState)
+        val pistonMode = processedState.getPistonValue()
 
         motor.setOutput(MotorControlMode.PERCENT_OUTPUT, motorVelocity)
         piston.set(pistonMode)
     }
 
-    // TODO: implement
-    private fun processState(state: RobotState): IntakeState = IntakeState.CLOSED
-
-    // TODO: implement
-    private fun getMotorVel(state: IntakeState): Float = defaultSpeed
-
-    // TODO: implement
-    private fun getPistonMode(state: IntakeState): Value = Value.kOff
+    private fun processState(state: RobotState): IntakeState = when (state) {
+        RobotState.INTAKE -> Collecting
+        RobotState.FEEDER_INTAKE, RobotState.CLIMB -> Opened
+        else -> Closed
+    }
 
     override fun reset() {
         TODO("Not yet implemented")
--- src/main/kotlin/frc/robot/subsystems/intake/IntakeState.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeState.kt
@@ -1,5 +1,23 @@
 package frc.robot.subsystems.intake
 
-enum class IntakeState {
-    CLOSED, OPENED, COLLECTING
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value
+
+interface IntakeState {
+    fun getPistonValue(): Value
+    fun getMotorOutput(): Float
+}
+
+
+object Closed : IntakeState {
+    override fun getPistonValue(): Value = Value.kReverse
+    override fun getMotorOutput(): Float = 0f
+}
+
+object Opened : IntakeState {
+    override fun getPistonValue(): Value = Value.kForward
+    override fun getMotorOutput(): Float = 0f
+}
+
+object Collecting : IntakeState by Opened {
+    override fun getMotorOutput(): Float = defaultSpeed
 }
