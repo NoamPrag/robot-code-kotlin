4f2b39d78fe9e4cdb7654198f6c5d0108689b5d3
Noam Prag <noam.prag0@gmail.com>
2021-08-28 16:26:25 +0300

    Applied deadband function in Axis

--- src/main/kotlin/frc/robot/controllers/Axis.kt
+++ src/main/kotlin/frc/robot/controllers/Axis.kt
@@ -2,19 +2,19 @@ package frc.robot.controllers
 
 import edu.wpi.first.wpilibj.Joystick
 import kotlin.math.abs
+import kotlin.math.sign
 
 typealias AxisListener = (Float) -> Unit
 
 class Axis(private val joystick: Joystick, private val index: Int) {
-    var deadband: Float = 0f
+    var deadband: (Float) -> Float = { it }
     var interpolation: (Float) -> Float = { it }
 
-    fun getRaw(): Float = joystick.getRawAxis(index).toFloat()
+    val raw: Float get() = joystick.getRawAxis(index).toFloat()
 
     fun get(): Float {
-        val rawValue = joystick.getRawAxis(index).toFloat()
-
-        return if (abs(rawValue) >= deadband) interpolation(rawValue) else 0f
+        val rawValue: Float = raw
+        return interpolation(deadband(abs(rawValue)) * sign(rawValue))
     }
 
     private val listeners: Collection<AxisListener> = mutableListOf()
